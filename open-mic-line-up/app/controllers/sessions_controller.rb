f10e46240a6d9e662b692cf5b0015a6adc444e81
ZenMindedBiz <35556570+ZenMindedBiz@users.noreply.github.com>
2020-06-30 17:27:00 -0600

    add "/" to routes in SessionsController

--- open-mic-line-up/app/controllers/sessions_controller.rb
+++ open-mic-line-up/app/controllers/sessions_controller.rb
@@ -2,32 +2,39 @@ class SessionsController < ApplicationController
 
 # routes and views to render login form to user
   get '/users/login' do
+    # redirect_if_logged_in
     erb :'users/login'
   end
 
 # this route's purpose is to receive the login form
 # find the user, and login the user (create a session)
-  post 'users/login' do
+  post '/users/login' do
     # Find the user
-    @user = User.find_by(:cellphone => params[:cellphone])
+    @user = User.find_by(cellphone: params[:cellphone])
     # Authenticate the user 
     # Login the user
     # Redirect to the user's landing page
-    if user && user.authenticate(params[:password])
+    if @user.authenticate(params[:password])
       # if truthy value, login the user and create their session
-      session[:user_id] = user.id
-      redirect to "/signups/performances"
+      session[:user_id] = @user.id
+      puts session
+      redirect "/users/#{@user.id}"
     else
       #tell user they entered invalid credentials
       #redirect to login page
       
-      redirect to '/users/signup'
+      redirect '/users/login'
     end
   end
 
-# routes and views to allow user to signup
-  get 'users/signup' do
-    erb :'users/signup'
+# routes and views to allow user to signup for Open Mic World
+  get '/users/signup' do
+    # erb :'users/signup'
+  end
+
+# user SHOW route where user signups for performances
+  get '/users/:id' do
+    "this will be the user show route"
   end
 
 end
--- open-mic-line-up/config.ru
+++ open-mic-line-up/config.ru
@@ -4,7 +4,10 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+# the next line allows sending PATCH and DELETE requests
 use Rack::MethodOverride
+
+# mount other controllers with "use"
 use SessionsController
 use SignupsController
 use UsersController
