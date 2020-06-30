02d10d313af035696ae1e5efe69b29f6c08462f9
ZenMindedBiz <35556570+ZenMindedBiz@users.noreply.github.com>
2020-06-30 15:48:52 -0600

    update login.erb

--- open-mic-line-up/app/controllers/sessions_controller.rb
+++ open-mic-line-up/app/controllers/sessions_controller.rb
@@ -2,12 +2,16 @@ class SessionsController < ApplicationController
 
 # routes and views to render login form to user
   get '/users/login' do
-    erb :login
+    erb :'users/login'
+  end
+
+  post 'users/login' do
+    
   end
 
 # routes and views to allow user to signup
   get 'users/signup' do
-    erb :signup
+    erb :'users/signup'
   end
 
 end
