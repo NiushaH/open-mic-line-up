775114ce248cd778d05ccd8ffe409c7c65967532
ZenMindedBiz <35556570+ZenMindedBiz@users.noreply.github.com>
2020-07-01 22:04:19 -0600

    update sessions_controller

--- open-mic-line-up/app/controllers/sessions_controller.rb
+++ open-mic-line-up/app/controllers/sessions_controller.rb
@@ -17,6 +17,7 @@ class SessionsController < ApplicationController
     # Redirect to the user's landing page
     if @user.authenticate(params[:password])
       # if truthy value, login the user and create their session
+# Q&A - why not build a method for begin a session??? and call here as well as for lines 44, 45, & 53??
       session[:user_id] = @user.id
       puts session
       redirect "/users/#{@user.id}"
@@ -38,7 +39,9 @@ class SessionsController < ApplicationController
     if params[:name] != "" && params[:cellphone] != "" && params[:password] != ""
       # valid input
       @user = User.create(params)
-binding.pry
+      # login user after signup
+      session[:user_id] = @user.id
+      puts session
       # runs a new HTTP GET request better than rendering erb template because
       #   separation of concerns -- it's the post route's job to create a new user, that's it (not to show the new user) 
       #   keeps it DRY to use a get request to get to a page,
@@ -50,7 +53,8 @@ binding.pry
     # Stretch Goal: refactor this to use ActiveRecord validations
     else
     # not valid input
-
+    # add message to tell user what is wrong
+    redirect '/users/signup'
     end
   end
 
@@ -59,7 +63,7 @@ binding.pry
 # the dynamic URL identifier gets stored in the params hash key along with the argument passed in as its value
   get '/users/:id' do
     @user = User.find_by(id: params[:id])
-    erb :'performances/performances'
+    erb :'/users/show'
   end
 
 #   post '/users/:id' do
@@ -74,7 +78,8 @@ binding.pry
 # LOGOUT ROUTE
   get '/users/logout' do
     # redirect_if_logged_out
-    erb :'users/logout'
+    session.clear
+    erb :'/'
   end
 
   post '/users/logout' do
